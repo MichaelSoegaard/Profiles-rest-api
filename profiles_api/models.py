07ab7ac238d7be32daedea1b88d9615f9b8beb2f
Michael Søegaard <michael@redpost.dk>
2022-03-24 09:00:58 +0100

    Setting up admin profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('Please supply an email address')
 
-        email = sel.normalize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
